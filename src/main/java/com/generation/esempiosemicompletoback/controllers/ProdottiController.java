aa266c516f0895fce3545ab156f4800078f4d7bb
StefanoR <rubinetti.stefano@gmail.com>
2025-09-25 10:39:40 +0200

    a

--- src/main/java/com/generation/esempiosemicompletoback/controllers/ProdottiController.java
+++ src/main/java/com/generation/esempiosemicompletoback/controllers/ProdottiController.java
@@ -6,6 +6,7 @@ import com.generation.esempiosemicompletoback.model.repository.ProdottoRepositor
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -18,19 +19,40 @@ public class ProdottiController
     @Autowired
     ProdottoRepository repo;
 
+    //http://localhost:8080/api/products?cat=i&v=3&sgn=true
     @GetMapping
-    public List<ProductGetDto> leggiTutti()
+    public List<ProductGetDto> leggiTutti
+    (
+            @RequestParam(required = false) String cat,
+            @RequestParam(required = false) Double v,
+            @RequestParam(required = false) Boolean sgn
+    )
     {
-       List<Prodotto> l = repo.findAll();
+        List<Prodotto> l = repo.findAll();
+        if(cat!=null)
+            l = l.stream().filter(p->p.getCategoria().toLowerCase().contains(cat.toLowerCase())).toList();
+        if(v!=null && sgn!=null)
+            l = l.stream()
+                    .filter
+                    (
+                            p->
+                            {
+                              if(sgn)
+                                  return p.getPrezzo()>=v;
+                              else
+                                  return p.getPrezzo()<=v;
+                            }
+                    ).toList();
 
-       List<ProductGetDto> listaDto = new ArrayList<>();
-
-       for(Prodotto p : l)
-       {
-           ProductGetDto convertito = new ProductGetDto(p);
-           listaDto.add(convertito);
-       }
-       return listaDto;
+        return l.stream().map(p-> new ProductGetDto(p)).toList();
+//       List<ProductGetDto> listaDto = new ArrayList<>();
+//
+//       for(Prodotto p : l)
+//       {
+//           ProductGetDto convertito = new ProductGetDto(p);
+//           listaDto.add(convertito);
+//       }
+//       return listaDto;
 //       return repo.findAll().stream().map(p-> new ProductGetDto(p)).toList();
     }
 
